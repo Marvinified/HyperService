188f4951b7f06d9583c9a1b4b4cf02e42fe0fc21
Marvin Tunji-ola <marvinsmart9@gmail.com>
2019-07-11 00:56:17 +0100

    event inteface done

--- src/kafka/bin/start.js
+++ src/kafka/bin/start.js
@@ -1,14 +1,20 @@
 const admin = require('./admin')
 const consume = require('./consume')
 const registerConsumers = require('../registerConsumers')
-// const fs = require('fs')
-// let topicsToCreate = fs.readFileSync('topics.json');
+const fs = require('fs')
+let topicsToCreate = fs.readFileSync(__dirname + '/../topics.json');
 
 module.exports = () => {
-    
+
     registerConsumers(consume)
+
+    const topics = JSON.parse(topicsToCreate)
     
     admin.listTopics((err, res) => {
-        console.log('topics', res);
+        console.log({ res, err });
+        console.log('Creating topics if not exist')
+        admin.createTopics(topics, (err, res) => {
+            console.log({ err, res })
+        })
     });
 }
--- src/kafka/registerConsumers.js
+++ src/kafka/registerConsumers.js
@@ -1,9 +1,9 @@
 module.exports = (consume) => {
+
     consume([
-        { topic: 'topic1', partitions: 0 }
+        { topic: 'log', partitions: 0 }
     ], ({ message, error }) => {
-        console.log("Consumer")
         console.log({ error, message })
-        // console.log('topic 1 event handle' + message)
     })
+
 }
