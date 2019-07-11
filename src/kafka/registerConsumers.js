66ca84514c90b88eee51bddb8102c028231b315e
Marvin Tunji-ola <marvinsmart9@gmail.com>
2019-07-11 18:20:17 +0100

    commit to pull

--- src/kafka/registerConsumers.js
+++ src/kafka/registerConsumers.js
@@ -6,4 +6,11 @@ module.exports = (consume) => {
         console.log({ error, message })
     })
 
+    consume([
+        {topic: 'newUserCreated', partitions: 0}
+    ], ({ message, error }) => {
+        console.log('send notification to the admin')
+        console.log({ error, message })
+    })
+
 }
